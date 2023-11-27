839a1e89d19cb0d4f82d0ceebd7bcf9e4a50699e
rcastellotti <me@rcastellotti.dev>
2023-11-27 14:42:03 +0100

    removed fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias gbr='xdg-open $(git remote get-url origin | sed -e "s/git\@/https:\/\//;s/:/\//2") &>/dev/null'
 
-
+set -U fish_greeting ""
 
